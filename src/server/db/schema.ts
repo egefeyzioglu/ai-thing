67c5c569d50006d643c436e809f8568fa14f0dd2
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2026-04-27 21:11:10 -0400

    Remove example post stuff

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -12,16 +12,3 @@ import { index, sqliteTableCreator } from "drizzle-orm/sqlite-core";
  */
 export const createTable = sqliteTableCreator((name) => `ai-thing_${name}`);
 
-export const posts = createTable(
-  "post",
-  (d) => ({
-    id: d.integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
-    name: d.text({ length: 256 }),
-    createdAt: d
-      .integer({ mode: "timestamp" })
-      .default(sql`(unixepoch())`)
-      .notNull(),
-    updatedAt: d.integer({ mode: "timestamp" }).$onUpdate(() => new Date()),
-  }),
-  (t) => [index("name_idx").on(t.name)],
-);
