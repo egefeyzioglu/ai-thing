e685c797e7ef693b1d90e4cd55c576fd13e4f1e3
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2026-05-20 05:20:58 -0400

    Revert "Normalize prompt group and local storage formatting"
    This reverts commit 9a43f28729c8551fa1fd395eeca3c9ac17307894.

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -10,7 +10,8 @@ export const localStorageSchema = {
       z.object({
         userId: z.string(),
         projectId: z.string(),
-      })),
+      }),
+    ),
     key: "ai-thing.activeProjectByUser",
     defaultValue: [] as unknown,
     version: 2,
@@ -34,11 +35,19 @@ export const localStorageSchema = {
 }>;
 
 export type LocalStorageKey = keyof typeof localStorageSchema;
-export type LocalStorageValue<K extends LocalStorageKey> = z.infer<typeof localStorageSchema[K]['schema']>;
-type LocalStorageSetter<K extends LocalStorageKey> = (newVal: LocalStorageValue<K> | ((prev: LocalStorageValue<K>) => LocalStorageValue<K>) ) => void;
+export type LocalStorageValue<K extends LocalStorageKey> = z.infer<
+  (typeof localStorageSchema)[K]["schema"]
+>;
+type LocalStorageSetter<K extends LocalStorageKey> = (
+  newVal:
+    | LocalStorageValue<K>
+    | ((prev: LocalStorageValue<K>) => LocalStorageValue<K>),
+) => void;
 
-export function getLocalStorage<K extends LocalStorageKey>(key: K): LocalStorageValue<K> | undefined {
-  if(typeof window === "undefined") return undefined;
+export function getLocalStorage<K extends LocalStorageKey>(
+  key: K,
+): LocalStorageValue<K> | undefined {
+  if (typeof window === "undefined") return undefined;
 
   const storageKey = localStorageSchema[key].key;
   const valueSchema = localStorageSchema[key].schema;
@@ -46,18 +55,26 @@ export function getLocalStorage<K extends LocalStorageKey>(key: K): LocalStorage
   const rawValue = localStorage.getItem(storageKey);
   if (rawValue === null) return undefined;
 
-  try { // zod parse and JSON parse both throw
+  try {
+    // zod parse and JSON parse both throw
     return valueSchema.parse(JSON.parse(rawValue));
   } catch (err) {
-    console.error("Invalid local storage value shape. Resetting to default value", { key, err });
-    const defaultValue = localStorageSchema[key].defaultValue as LocalStorageValue<K>;
+    console.error(
+      "Invalid local storage value shape. Resetting to default value",
+      { key, err },
+    );
+    const defaultValue = localStorageSchema[key]
+      .defaultValue as LocalStorageValue<K>;
     localStorage.setItem(storageKey, JSON.stringify(defaultValue));
     return defaultValue;
   }
 }
 
-export function setLocalStorage<K extends LocalStorageKey>(key: K, value: LocalStorageValue<K>) {
-  if(typeof window === "undefined") return undefined;
+export function setLocalStorage<K extends LocalStorageKey>(
+  key: K,
+  value: LocalStorageValue<K>,
+) {
+  if (typeof window === "undefined") return undefined;
 
   const storageKey = localStorageSchema[key].key;
   const valueSchema = localStorageSchema[key].schema;
@@ -65,26 +82,38 @@ export function setLocalStorage<K extends LocalStorageKey>(key: K, value: LocalS
     const valueToWrite = JSON.stringify(valueSchema.parse(value));
     localStorage.setItem(storageKey, valueToWrite);
   } catch (err) {
-    console.error("Invalid value shape or error with JSON.stringify. Did not save to local storage", { key, err })
+    console.error(
+      "Invalid value shape or error with JSON.stringify. Did not save to local storage",
+      { key, err },
+    );
   }
 }
 
-export function useLocalStorage<K extends LocalStorageKey>(key: K): [LocalStorageValue<K>, LocalStorageSetter<K>] {
+export function useLocalStorage<K extends LocalStorageKey>(
+  key: K,
+): [LocalStorageValue<K>, LocalStorageSetter<K>] {
   const defaultValue = localStorageSchema[key].defaultValue;
   const [value, setValue] = useState(defaultValue);
 
   useEffect(() => {
     const storedValue = getLocalStorage(key);
-    if(storedValue !== undefined) {
+    if (storedValue !== undefined) {
       setValue(storedValue);
     }
   }, [key]);
 
   const setValueWrapper = useCallback(
-    (newVal: LocalStorageValue<K> | ((prev: LocalStorageValue<K>) => LocalStorageValue<K>)) => {
+    (
+      newVal:
+        | LocalStorageValue<K>
+        | ((prev: LocalStorageValue<K>) => LocalStorageValue<K>),
+    ) => {
       if (typeof newVal === "function") {
         setValue((prev: LocalStorageValue<K>) => {
-          const newValue = newVal(prev ?? localStorageSchema[key].defaultValue as LocalStorageValue<K>);
+          const newValue = newVal(
+            prev ??
+              (localStorageSchema[key].defaultValue as LocalStorageValue<K>),
+          );
           setLocalStorage(key, newValue);
           return newValue;
         });
@@ -93,7 +122,7 @@ export function useLocalStorage<K extends LocalStorageKey>(key: K): [LocalStorag
         setValue(newVal);
       }
     },
-    [key]
+    [key],
   );
   return [value as LocalStorageValue<K>, setValueWrapper];
 }
